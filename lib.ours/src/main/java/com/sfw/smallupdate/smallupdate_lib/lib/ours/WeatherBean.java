d31d48e639850e9d5a1ac396d8f0f65cb6032d03
shufengwu <sfwandroid@163.com>
2016-12-15 15:18:14 +0800

    完善

---
+++ lib.ours/src/main/java/com/sfw/smallupdate/smallupdate_lib/lib/ours/WeatherBean.java
@@ -0,0 +1,270 @@
+package com.sfw.smallupdate.smallupdate_lib.lib.ours;
+
+import java.util.List;
+
+/**
+ * 网络请求json字符串对应的JavaBean类
+ */
+public class WeatherBean {
+
+    private String msg;
+    private String retCode;
+    private List<ResultBean> result;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getRetCode() {
+        return retCode;
+    }
+
+    public void setRetCode(String retCode) {
+        this.retCode = retCode;
+    }
+
+    public List<ResultBean> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ResultBean> result) {
+        this.result = result;
+    }
+
+    public static class ResultBean {
+
+        private String airCondition;
+        private String city;
+        private String coldIndex;
+        private String updateTime;
+        private String date;
+        private String distrct;
+        private String dressingIndex;
+        private String exerciseIndex;
+        private String humidity;
+        private String province;
+        private String sunset;
+        private String sunrise;
+        private String temperature;
+        private String time;
+        private String washIndex;
+        private String weather;
+        private String week;
+        private String wind;
+        private List<FutureBean> future;
+
+        public String getAirCondition() {
+            return airCondition;
+        }
+
+        public void setAirCondition(String airCondition) {
+            this.airCondition = airCondition;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getColdIndex() {
+            return coldIndex;
+        }
+
+        public void setColdIndex(String coldIndex) {
+            this.coldIndex = coldIndex;
+        }
+
+        public String getUpdateTime() {
+            return updateTime;
+        }
+
+        public void setUpdateTime(String updateTime) {
+            this.updateTime = updateTime;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getDistrct() {
+            return distrct;
+        }
+
+        public void setDistrct(String distrct) {
+            this.distrct = distrct;
+        }
+
+        public String getDressingIndex() {
+            return dressingIndex;
+        }
+
+        public void setDressingIndex(String dressingIndex) {
+            this.dressingIndex = dressingIndex;
+        }
+
+        public String getExerciseIndex() {
+            return exerciseIndex;
+        }
+
+        public void setExerciseIndex(String exerciseIndex) {
+            this.exerciseIndex = exerciseIndex;
+        }
+
+        public String getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(String humidity) {
+            this.humidity = humidity;
+        }
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        public String getSunset() {
+            return sunset;
+        }
+
+        public void setSunset(String sunset) {
+            this.sunset = sunset;
+        }
+
+        public String getSunrise() {
+            return sunrise;
+        }
+
+        public void setSunrise(String sunrise) {
+            this.sunrise = sunrise;
+        }
+
+        public String getTemperature() {
+            return temperature;
+        }
+
+        public void setTemperature(String temperature) {
+            this.temperature = temperature;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getWashIndex() {
+            return washIndex;
+        }
+
+        public void setWashIndex(String washIndex) {
+            this.washIndex = washIndex;
+        }
+
+        public String getWeather() {
+            return weather;
+        }
+
+        public void setWeather(String weather) {
+            this.weather = weather;
+        }
+
+        public String getWeek() {
+            return week;
+        }
+
+        public void setWeek(String week) {
+            this.week = week;
+        }
+
+        public String getWind() {
+            return wind;
+        }
+
+        public void setWind(String wind) {
+            this.wind = wind;
+        }
+
+        public List<FutureBean> getFuture() {
+            return future;
+        }
+
+        public void setFuture(List<FutureBean> future) {
+            this.future = future;
+        }
+
+        public static class FutureBean {
+
+            private String date;
+            private String dayTime;
+            private String night;
+            private String temperature;
+            private String week;
+            private String wind;
+
+            public String getDate() {
+                return date;
+            }
+
+            public void setDate(String date) {
+                this.date = date;
+            }
+
+            public String getDayTime() {
+                return dayTime;
+            }
+
+            public void setDayTime(String dayTime) {
+                this.dayTime = dayTime;
+            }
+
+            public String getNight() {
+                return night;
+            }
+
+            public void setNight(String night) {
+                this.night = night;
+            }
+
+            public String getTemperature() {
+                return temperature;
+            }
+
+            public void setTemperature(String temperature) {
+                this.temperature = temperature;
+            }
+
+            public String getWeek() {
+                return week;
+            }
+
+            public void setWeek(String week) {
+                this.week = week;
+            }
+
+            public String getWind() {
+                return wind;
+            }
+
+            public void setWind(String wind) {
+                this.wind = wind;
+            }
+        }
+    }
+}
